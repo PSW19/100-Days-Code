aa8a4b6de9d11d7db5fb073e38c1d5d27ffc9db3
PSW19 <114101489+PSW19@users.noreply.github.com>
2022-09-24 23:52:50 +0530

    Simple Addition

---
+++ Day3.py
@@ -0,0 +1,5 @@
+print ("Please input 2 numbers")
+num1 = int(input('Enter first digit '))
+num2 = int(input('Enter second digit '))
+result = num1 + num2 
+print("{0}+{1} = {2}".format(num1,num2,result))
