749a0f155b726a090fa97a52cecbfdf1a9c54e78
PSW19 <114101489+PSW19@users.noreply.github.com>
2022-09-27 23:45:34 +0530

    Day 6 Brand Name Generator

---
+++ Day 6 Brand Name Generator.py
@@ -0,0 +1,5 @@
+print('awelcome to Band Name generator!')
+city = input('Name of a the city you live in')
+pet = input('Type of pet you like to have')
+band_name = city + '' + pet
+print ("The Band Name is :"+ band_name)
