3d5f3552e383d2f580c3e437b84b16d566f21994
PSW19 <114101489+PSW19@users.noreply.github.com>
2022-10-04 23:54:42 +0530

    Day 12 Celsius to Fahrenheit

---
+++ Day 12 Celsius to Fahrenheit.py
@@ -0,0 +1,3 @@
+Celsius = float(input("Temperature value in degree Celsius"))
+Fahrenheit = (Celsius * 1.8) + 32
+print('The %.2f degree Celsius is equal to: %.2f Fahrenheit' %(Celsius, Fahrenheit))
